65601bf12a309ea174f24debb6caca69d1ef6e4f
Byjus5 <89545408+Byjus5@users.noreply.github.com>
2021-12-01 11:18:46 -0600

    Update CreateStory.js

--- screens/CreateStory.js
+++ screens/CreateStory.js
@@ -5,7 +5,7 @@ export default class CreateStory extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Create Story</Text>
+        <Text>Crear historia</Text>
       </View>
     );
   }
